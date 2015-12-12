d8cfbe21e1735f31e3e28b60d8f89339232f99c6
Sean Graham <seanmgrahamnj@yahoo.com>
2015-12-12 14:31:09 -0500

    added baskets and order controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   mount Shoppe::Engine => "/shoppe"
-  
+
   get "product/:permalink", to: "products#show", as: "product"
   post "product/:permalink", to: "products#buy", as: "buy"
   root to: "products#index"
+
+  post "product/:permalink", to: "products#buy"
+
+  get "basket", to: "orders#show"
+  delete "basket", to: "orders#destroy"
+
 end
